d47130d3a09c21b537aa1697c93bce75ae23bce0
smiton1 <182565156+smiton1@users.noreply.github.com>
2024-11-28 12:48:53 -0600

    refactored some code, added some functionality, working on logic

--- app.js
+++ app.js
@@ -1,49 +1,70 @@
-let gameboard = [0,1,2,3,4,5,6,7,8]
-
+const myArray = [1,2,3,4,5,6,7,8,9]
+let playerArr = []
+let computerArr = []
+const winningArrays = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]
+]
+let player1 = {
+    name: "player 1",
+    marker: "X",
+}
+let player2 = {
+    name: "player 2",
+    marker: "O",
+}
+const gameBoard = document.querySelector(".gameBoard")
+const box = gameBoard.childNodes
+let playing = false 
 const game = (function(){
-    let playing = false 
-    let div = document.querySelectorAll(".box")
-    const gameBoard = document.querySelector(".gameBoard")
+    let gameboard = [0,1,2,3,4,5,6,7,8]
     const start = document.querySelector(".start")
     start.addEventListener("click",()=>{
-        console.log("clicked")
-        if(playing === false){
-            gameboard.forEach(element => {
-                const div = document.createElement("div")
-                div.setAttribute("id", gameboard[element])
-                div.classList.add("box")
-                div.addEventListener("click",()=>{
-                    div.textContent = player1.marker
-                    player2.play()
-                })
-                gameBoard.append(div)
-                playing = true
-            });
-        }
-    })
-    let player1 = {
-        name: "player 1",
-        marker: "X",
-
-    }
-    let player2 = {
-        name: "player 2",
-        marker: "O",
-        play(){
-            let cpu = Math.floor((Math.random() * gameboard.length)+1)
-            let targetDiv = document.getElementById(cpu)
+        if(playing=== false){
+            playing= true
             gameboard.forEach(element=>{
-                if(targetDiv.innerHTML != ""){
-                    let cpu = Math.floor((Math.random() * gameboard.length)+1)
-                    let targetDiv = document.getElementById(cpu)
-                }else{
-                    targetDiv.textContent= player2.marker
-                }
+                let box = document.createElement("div")
+                box.classList.add('box')
+                box.setAttribute("id", gameboard[element])
+                gameBoard.appendChild(box)
+                box.addEventListener("click",()=>{
+                    box.textContent= player1.marker
+                    playerArr.push(box.id)
+                    console.log(playerArr)
+                    computer()
+                    checkScore()
+                })  
             })
         }
-    }
-
-    console.log(player1.name, player1.marker)
-
+    })   
 
 })()
+
+function checkScore(){
+    winningArrays.forEach((arr)=>{
+        if(playerArr.toString()===arr.toString()){
+            console.log("player wins")
+        }
+        if(computerArr.toString()===arr.toString()){
+            console.log("computer wins")
+        }
+    })
+}
+function randomNum(){
+    let num = Math.floor(Math.random()*9)
+    return num
+}
+
+
+function computer(){
+    let num = randomNum()
+    box[num].textContent ==""
+    computerArr.push(num)
+    box[num].textContent = player2.marker
+}
